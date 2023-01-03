eee84de74eaa4f6c3e47056d41bf99804b8934ab
AlexGuSpb <spbalexgus@yandex.ru>
2023-01-03 15:05:46 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexey Gusev");
+        System.out.println("15.12.1988");
+    }
+}
