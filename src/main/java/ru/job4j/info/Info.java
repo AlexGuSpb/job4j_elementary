7d0d94649d74d5ed519a3c3989df814dec4836b9
AlexGuSpb <spbalexgus@yandex.ru>
2023-01-04 10:26:34 +0300

    4.2. Заголовок в файле java исправление2 [#159036 #377103]

--- src/main/java/ru/job4j/info/Info.java
+++ src/main/java/ru/job4j/info/Info.java
@@ -2,7 +2,6 @@ package ru.job4j.info;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("03.01.2023"); /*странно, делаю все, как написано,
-        но на гит новый коммит грузится без исправления.*/
+        System.out.println("03.01.2023");
     }
 }
